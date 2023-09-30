1b1bc46699070f1e35070db85e81ae686257d88b
Khushal Neekhra <ng05031975@gmail.com>
2023-09-30 20:51:27 +0530

    funtionality complted ui building remian

--- To-Do_list_App/Python/To-Do_list.py
+++ To-Do_list_App/Python/To-Do_list.py
@@ -1,9 +1,6 @@
 from tkinter import messagebox
-import tkinter as tk
+from tkinter import *
 import sqlite3 as sql
-window = tk.Tk()
-canvas = tk.Canvas(window , width = 400 , height= 300 )
-canvas.pack()
 
 def add_task():
     task_string = task_field.get()
@@ -31,14 +28,46 @@ def delete_task():
         messagebox.showinfo("error" , "no task selected cannot delete")
 
 def delete_all_tasks():
-    pass
-    #code is under devlopement not finished yet
+    try:
+        the_value = task_listbox.get(task_listbox.curselection())
+        if the_value in tasks:
+            task.remove(the_value)
+            list_update()
+            the_cursor.execute("Delete from task where title = ?",(the_value))
+    except:
+        messagebox.showinfo("Error","No task selected . cannot delete")
 
-labeltitle = tk.Label(window, text='To - Do List')
-labeltitle.config(font=('helvetica', 20))
-canvas.create_window(200, 25, window=labeltitle)
+def delete_all_tasks():
+    messagebox = messagebox.askyesno("Delete All" , "Are you sure ?")
+    if messagebox == True:
+        while(len(task) != 0):
+            task.pop()
+        the_cursor.execute("delete from task")
+        list_update()
 
-entry = tk.Entry(window) 
-canvas.create_window(200, 75, window=entry ,width = 300)
+def clear_list():
+    task_listbox.delete(0,"delete from task")
 
-window.mainloop()
+def close():
+    print(tasks)
+    window.destroy()
+
+def retrieve_database():
+    while(len(tasks) != 0):
+        task.pop()
+    for row in the_cursor.execute("select title from tasks"):
+        task.append(row[0])
+if __name__ == "__main__":
+    window = Tk()
+    canvas = Frame(window , width = 400 , height= 500 )
+    canvas.pack()
+    the_connection = sql.connect("listoftasks.db")
+    the_cursor = the_connection.cursor()
+    the_cursor.execute("create table if not exists tasks (title here)")
+    tasks = []
+    lable = Label(canvas,text= "To - Do List",font=( "arial" ,25 , "bold"), foreground= "black")
+    entry_box = Entry(canvas , font= ("arial" , 14) , foreground= "black" , background= "white")
+
+    lable.place(x = 110 , y = 20)
+
+    window.mainloop()
